444e533af1c590768c3447a7d7c648bc0ea5cb87
ipapMaster <wmast@inbox.ru>
2023-10-05 19:56:17 +0300

    Bot Example #2

--- bot_2.py
+++ bot_2.py
@@ -34,19 +34,39 @@ def switch(message):
                      reply_markup=markup)
 
 
-@bot.message_handler(content_types=['text'])
-def default_test(message):
-    markup = types.InlineKeyboardMarkup()
-    swt_but = types.InlineKeyboardButton(text='Выбор сайта',
-                                         url='https://mail.ru')
-    markup.add(swt_but)
-    bot.send_message(message.chat.id,
-                     'Перейти на Mail.ru',
-                     reply_markup=markup)
-
 @bot.message_handler(commands=['button'])
 def button_message(message):
     """Вариант создания меню"""
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton('/start')
+    btn2 = types.KeyboardButton('/switch')
+    btn3 = types.KeyboardButton('/url')
+    btn4 = types.KeyboardButton('Кнопка')
+    markup.add(btn1, btn2, btn3, btn4)
+    bot.send_message(message.chat.id, 'Ваш выбор', reply_markup=markup)
+
+
+@bot.message_handler(content_types=['text'])
+def message_reply(message):
+    if message.text == 'Кнопка':
+        print('dsfsdfsd')
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton('Кнопка 2')
+        markup.add(btn1)
+        bot.send_message(message.chat.id,
+                         'Что нужно?', reply_markup=markup)
+    elif message.text == 'Кнопка 2':
+        bot.send_message(message.chat.id,
+                         'Спасибо за нажатие кнопки №2')
+        bot.send_message(message.chat.id, '/button')
+    else:
+        markup = types.InlineKeyboardMarkup()
+        swt_but = types.InlineKeyboardButton(text='Выбор сайта',
+                                             url='https://mail.ru')
+        markup.add(swt_but)
+        bot.send_message(message.chat.id,
+                         'Перейти на Mail.ru',
+                         reply_markup=markup)
 
 
 # бот работает непрерывно
